dfa833e48d7f17c29570714623a247914260c067
gabrielgcmr <gabrielcactus@gmail.com>
2022-12-27 16:30:48 -0300

    bug fixed

--- src/Controllers/characterControllers.ts
+++ src/Controllers/characterControllers.ts
@@ -2,7 +2,7 @@ import {Request,Response } from "express"
 import Character, { ICharacter } from "../Models/character";
 //------------------------------------------------------------------------------------//
 //========================|Pegar todos os Personagens do banco|===========================================//
-const getAllCharacters = (req:Request, res:Response) => {
+export const getAllCharacters = (req:Request, res:Response) => {
     Character.find({}, (err:Error,data:ICharacter) => {
         if(err) {                                                                   //aprovado!
             return res.json({Error:err});
@@ -12,7 +12,7 @@ const getAllCharacters = (req:Request, res:Response) => {
 };
 //------------------------------------------------------------------------------------//
 //========================|Criar um novo personagem|===========================================//
-const newCharacter = (req:Request,res:Response) => {
+export const newCharacter = (req:Request,res:Response) => {
 Character.findOne({nome: req.body.nome }, (err:Error, data:ICharacter) => {
         if(!data) {
             const newPerson = new Character(req.body)                                  //aprovado!
@@ -28,7 +28,7 @@ Character.findOne({nome: req.body.nome }, (err:Error, data:ICharacter) => {
 };
 //------------------------------------------------------------------------------------//
 //========================|!DELETAR TODOS OS PERSONAGENS!|===========================================//
-const deleteAllCharacter = (req:Request, res:Response) => {
+export const deleteAllCharacter = (req:Request, res:Response) => {
     Character.deleteMany({}, err => {
         if(err) {
           return res.json({message: "Complete delete failed"});                    //aprovado!
@@ -38,7 +38,7 @@ const deleteAllCharacter = (req:Request, res:Response) => {
 };
 //------------------------------------------------------------------------------------//
 //========================|Pegar uma pesonagem pelo nome|===========================================//
-const getOneCharacter = (req:Request, res:Response) => {
+export const getOneCharacter = (req:Request, res:Response) => {
     let nome = req.params.nome; 
 
     Character.findOne({nome:nome}, (err:Error, data:ICharacter) => {             //aprovado!
@@ -52,7 +52,7 @@ const getOneCharacter = (req:Request, res:Response) => {
 };
 //------------------------------------------------------------------------------------//
 //========================|Deleta um personagem pelo nome|===========================================//
-const deleteOneCharacter = (req:Request, res:Response) => {
+export const deleteOneCharacter = (req:Request, res:Response) => {
     const nome = req.params.nome
 
      Character.deleteOne({nome:nome}, (err:Error, data:ICharacter) => {
@@ -62,7 +62,7 @@ const deleteOneCharacter = (req:Request, res:Response) => {
     }
    
     else if (err) {
-        return res.json(`Alguma dedu errado, tente novamente. ${err}`);
+        return res.json(`Algum deu errado, tente novamente. ${err}`);
     }
     else {
         return res.json({message: "Personagem deletado com sucesso"});
@@ -72,4 +72,4 @@ const deleteOneCharacter = (req:Request, res:Response) => {
 
 
 //------------------------------------------------------------------------------------//
-export default {getAllCharacters, newCharacter,deleteAllCharacter,getOneCharacter,deleteOneCharacter} 
+
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,7 @@
 //============================|(IMPORT'S)|=====================================//
 import { Router } from "express"
 import express, { Request, Response }  from "express";
-import controllerRoutes from "../Controllers/controllers";
+import * as characterController from '../Controllers/characterControllers'
 import multer from "multer";
 //============================|(Const's)|=====================================//
 const upload = multer()
@@ -12,12 +12,12 @@ routes.get('/',(req:Request, res:Response) =>{
     res.json({message:`Seja bem vindo novamente ${Deus}`})
 })
 //============================|(Rotas)|=====================================//
-routes.get('/personagens', controllerRoutes.getAllCharacter);
-routes.post('/create', upload.none(), controllerRoutes.newCharacter);
-routes.delete('/deleteall', controllerRoutes.deleteAllCharacter);
+routes.get('/personagens', characterController.getAllCharacters);
+routes.post('/create', upload.none(), characterController.newCharacter);
+routes.delete('/deleteall', characterController.deleteAllCharacter);
 
- routes.get('/personagem/:nome', controllerRoutes.getOneCharacter);
- routes.delete('/delete/:nome', controllerRoutes.deleteOneCharacter);
+ routes.get('/personagem/:nome', characterController.getOneCharacter);
+ routes.delete('/delete/:nome', characterController.deleteOneCharacter);
 //  routes.put('/personagem/:nome', controllerRoutes.updateCharacter);
 //--------------------------------------------------------------------------//
 export default routes     // exportando rotas por padrão.
