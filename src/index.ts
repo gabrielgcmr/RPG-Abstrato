8f9553546e042fc10201fd736950b6c54d57e6e4
gabrielgcmr <gabrielcactus@gmail.com>
2022-12-29 09:18:53 -0300

    corrections

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,21 @@
 import  Express, { Router }  from "express"
 import routes from "./routes/routes"
 import connectToMongoDB from './config/database/databaseconfig'
-import { savePerson } from "./test/testes2"
 import cors from 'cors'
 
-connectToMongoDB()
 const app = Express()
-app.use(express.json()) // Pedido pra q o express aceite "Json"
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST")
-    app.use(cors())
-    next()
-})
-
-
-app.use(routes)
-
+connectToMongoDB()
 app.use(Express.json()) // Pedido pra q o express aceite "Json"
+app.use(routes)
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST")
     app.use(cors())
     next()
 })
-
-savePerson()
 
 app.listen(3000, ()=>{
     console.log(`Rodando na porta 3000`)
 })
-
-
-
-
 
 export default app
